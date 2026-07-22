1f51bd753b07b82f589b30c800c3a1095b55d98c
Edgar Hofmann <43002113+ehofmannbr@users.noreply.github.com>
2026-07-22 09:52:02 -0300

    Add files via upload
    Replacing vgapmod_06

---
+++ vgapmod_06.v
@@ -0,0 +1,177 @@
+/*
+ * Copyright (c) 2026 Edgar Hofmann
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// `default_nettype none
+// vgapmod_04 using TT prototype and Digilent VGA Pmod
+
+module vgapmod_06(
+//  input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+//  input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+//  output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+//  input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+// VGA signals
+wire hsync;
+wire vsync;
+wire [1:0] R;
+wire [1:0] G;
+wire [1:0] B;
+wire video_active;
+wire [9:0] pix_x;
+wire [9:0] pix_y;
+
+// Tile Registers
+reg[11:0] _cor;             // Saida do mux de cor
+reg[11:0] cor1;             // Registro de cor
+reg[11:0] cor2;             // Registro de cor
+reg[11:0] cor3;             // Registro de cor
+reg[11:0] cor4;             // Registro de cor
+reg[11:0] cor5;             // Registro de cor
+reg[11:0] cor6;             // Registro de cor
+reg[11:0] cor7;             // Registro de cor
+reg[11:0] cor8;             // Registro de cor
+reg[11:0] cor9;             // Registro de cor
+reg[11:0] cort;             // Registro de cor
+reg[25:0] t_cnt;            // Enables period > 1s
+
+// TinyVGA PMOD
+assign uo_out = {hsync, B[0], G[0], R[0], vsync, B[1], G[1], R[1]};
+  
+// Unused outputs assigned to 0.
+// Assign uio_out to R0,R1,G0,G1,B0,B1,X,X
+   assign uio_out = 0;
+// assign uio_oe  = 0;
+
+// Suppress unused signals warning
+// wire _unused_ok = &{ena, ui_in, uio_in};
+
+reg [9:0] counter;
+reg clk25;
+
+hvsync_generator hvsync_gen(
+  .clk(clk25),
+  .reset(~rst_n),
+  .hsync(hsync),
+  .vsync(vsync),
+  .display_on(video_active),
+  .hpos(pix_x),
+  .vpos(pix_y)
+);
+
+parameter XL = 213;
+parameter XM = 426;
+parameter XR = 640;
+parameter YU = 160;
+parameter YM = 320;
+parameter YB = 480;
+
+// Tiles
+always @(*) begin
+   if ((pix_x>=0)&&(pix_x<XL)&&(pix_y>=0)&&(pix_y<YU)) 
+      begin
+      _cor = cor1;
+      end
+	else
+   if ((pix_x>=XL)&&(pix_x<XM)&&(pix_y>=0)&&(pix_y<YU)) 
+      begin
+      _cor = cor2;
+		end
+	else
+   if ((pix_x>=XM)&&(pix_x<XR)&&(pix_y>=0)&&(pix_y<YU)) 
+      begin
+      _cor = cor3;
+		end
+	else
+   if ((pix_x>=0)&&(pix_x<XL)&&(pix_y>=YU)&&(pix_y<YM)) 
+      begin
+      _cor = cor4;
+      end
+	else
+   if ((pix_x>=XL)&&(pix_x<XM)&&(pix_y>=YU)&&(pix_y<YM)) 
+      begin
+      _cor = cor5;
+		end
+	else
+   if ((pix_x>=XM)&&(pix_x<XR)&&(pix_y>=YU)&&(pix_y<YM)) 
+      begin
+      _cor = cor6;
+		end
+	else
+   if ((pix_x>=0)&&(pix_x<XL)&&(pix_y>=YM)&&(pix_y<YB)) 
+      begin
+      _cor = cor7;
+      end
+	else
+   if ((pix_x>=XL)&&(pix_x<XM)&&(pix_y>=YM)&&(pix_y<YB)) 
+      begin
+      _cor = cor8;
+		end
+	else
+   if ((pix_x>=XM)&&(pix_x<XR)&&(pix_y>=YM)&&(pix_y<YB)) 
+      begin
+      _cor = cor9;
+		end
+	else
+	  begin
+	  _cor = 12'b0;
+	  end
+end		
+
+always @(posedge clk) begin
+     clk25 = ~clk25;
+  end
+
+always @(posedge clk25) begin
+   if (!rst_n)
+      begin
+		t_cnt  = 0;
+		cor1   = 12'b000000000000;
+		cor2   = 12'b111100000000;
+		cor3   = 12'b000011110000;
+		cor4   = 12'b000000001111;
+		cor5   = 12'b111111110000;
+		cor6   = 12'b111100001111;
+		cor7   = 12'b000011111111;
+		cor8   = 12'b011101110111;
+		cor9   = 12'b111111111111;
+      end
+   else
+      begin
+      if (t_cnt==24999999) // 1 em 1 segundo
+		     begin
+			  t_cnt = 0;
+			  cort = cor9;
+			  cor9 = cor8;
+			  cor8 = cor7;
+			  cor7 = cor6;
+			  cor6 = cor5;
+			  cor5 = cor4;
+			  cor4 = cor3;
+			  cor3 = cor2;
+			  cor2 = cor1;
+			  cor1 = cort;
+			  end
+		  else
+		     begin
+			  t_cnt = t_cnt + 1'b1;
+			  end  
+        end
+end
+
+assign R[1]=_cor[11];
+assign R[0]=_cor[10];
+assign G[1]=_cor[7];
+assign G[0]=_cor[6];
+assign B[1]=_cor[3];
+assign B[0]=_cor[2];
+
+endmodule
+
+
